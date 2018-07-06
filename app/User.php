3e19946d93d3c9f9788103d3e3f046c02d90cc3e
Kiran <ignkiran@gmail.com>
2018-07-06 08:59:54 +0530

    Admin page Setup and Style-Script implement

--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    // Relationship with Role; User roles
+    public function role(){
+        return $this->belongsTo('App\Role');
+    }
 }
---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var elixir =  require('laravel-elixir');
+
+elixir(function(mix){
+    mix.styles([
+            'bootstrap.css',
+            'font-awesome.css',
+            'metisMenu.css',
+            'sb-admin-2.css'
+        ],'./public/css/libs.css')
+        .scripts([
+            'jquery.js',
+            'bootstrap.js',
+            'metisMenu.js',
+            'sb-admin-2.js',
+            'app.js'
+        ],'./public/js/libs.js')
+});
